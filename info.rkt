dcab19016c912b6f59fe74095d89edae3f6b6394
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-12-01 15:59:35 +0700

    Add pprint dep; remove data subdir

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "license-audit")
-(define deps '("base" "text-table"))
+(define deps '("base" "text-table" "pprint"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/license-audit.scrbl" ())))
 (define pkg-desc "audit package licenses (including their dependencies)")
